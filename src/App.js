69975abbbcebf4e0395962078daea3d62a6afa3b
webstoryboy <webstupids@gmail.com>
2022-07-24 09:59:19 +0900

    222

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from './components/pages/Main';
+import About from './components/pages/About';
+import Youtube from './components/pages/Youtube';
+import Movie from './components/pages/Movie';
+import Portfolio from './components/pages/Portfolio';
+import Reference from './components/pages/Reference';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/youtube" element={<Youtube />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/refer" element={<Reference />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
